f3b3144416a789890f764f4878980bdce760303d
Cannon Spears <cannonspears@gmail.com>
2023-12-04 17:54:31 -0600

    Title Metadata

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "notesGPT | Home",
+};
+
 export default function NotesPage() {
   return <div>Notes Page</div>;
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "notesGPT | Sign Up",
+};
 
 export default function SignUpPage() {
   return (
