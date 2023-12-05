b9f2252a1b5218ae3ff8a75f13b1f280553fb833
Cannon Spears <cannonspears@gmail.com>
2023-12-05 10:59:42 -0600

    Validation

---
+++ src/app/api/notes/route.ts
@@ -0,0 +1,26 @@
+import { createNoteSchema } from "@/lib/validation/note";
+import { auth } from "@clerk/nextjs";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const parseResult = createNoteSchema.safeParse(body);
+
+    if (!parseResult.success) {
+      console.error(parseResult.error);
+      return Response.json({ error: "Invalid input" }, { status: 400 });
+    }
+
+    const { title, content } = parseResult.data;
+
+    const { userId } = auth();
+
+    if (!userId) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
