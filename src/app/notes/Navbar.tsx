b099c75b1816db4b11fd28b5af555a49bc0ed389
Cannon Spears <cannonspears@gmail.com>
2023-12-05 10:09:47 -0600

    Add UserButton and Add Note

--- src/app/notes/Navbar.tsx
+++ src/app/notes/Navbar.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/assets/logo.png";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 
 export default function NavBar() {
   return (
@@ -10,7 +13,18 @@ export default function NavBar() {
           <Image src={logo} alt="notesGPT logo" width={40} height={40} />
           <span className="font-bold">notesGPT</span>
         </Link>
-        <span>Element 2</span>
+        <div className="flex items-center gap-2">
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+            }}
+          />
+          <Button className="pl-2">
+            <Plus size={20} className="mr-2" />
+            Add Note
+          </Button>
+        </div>
       </div>
     </div>
   );
