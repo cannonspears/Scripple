3fbca16264d6ce0f01529d6c81013cde20a560a9
Cannon Spears <cannonspears@gmail.com>
2023-12-05 11:29:45 -0600

    Create note schema

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,3 +1,4 @@
+import prisma from "@/lib/db/prisma";
 import { createNoteSchema } from "@/lib/validation/note";
 import { auth } from "@clerk/nextjs";
 
@@ -19,8 +20,18 @@ export async function POST(req: Request) {
     if (!userId) {
       return Response.json({ error: "Unauthorized" }, { status: 401 });
     }
+
+    const note = await prisma.note.create({
+      data: {
+        title,
+        content,
+        userId,
+      },
+    });
+
+    return Response.json({ note }, { status: 201 });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
   }
 }
