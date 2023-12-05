a1615ae64a84ca6ad99d83d435a9e7ab3bb030ec
Cannon Spears <cannonspears@gmail.com>
2023-12-05 10:32:23 -0600

    Redirect if already logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 import logo from "@/assets/logo.png";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { userId } = auth();
+
+  if (userId) redirect("/notes");
+
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-5">
       <div className="flex items-center gap-4">
