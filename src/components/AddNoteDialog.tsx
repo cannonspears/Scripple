3dd869afaf92b534a857c24c683df80309a6990e
Cannon Spears <cannonspears@gmail.com>
2023-12-05 13:17:27 -0600

    Input title and content

--- src/components/AddNoteDialog.tsx
+++ src/components/AddNoteDialog.tsx
@@ -1,7 +1,23 @@
 import { CreateNoteSchema, createNoteSchema } from "@/lib/validation/note";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 interface AddNoteDialogProps {
   open: boolean;
@@ -23,6 +39,36 @@ export default function AddNoteDialog({ open, setOpen }: AddNoteDialogProps) {
         <DialogHeader>
           <DialogTitle>Add Note</DialogTitle>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Note title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note Content</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Note content" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
