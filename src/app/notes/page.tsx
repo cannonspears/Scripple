ffb11f95e54b23de36d6f0cd114c3fd77c054944
Cannon Spears <cannonspears@gmail.com>
2023-12-05 11:34:26 -0600

    findMany notes from prisma

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,9 +1,17 @@
+import { auth } from "@clerk/nextjs";
+import prisma from "@/lib/db/prisma";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "notesGPT | Home",
 };
 
-export default function NotesPage() {
-  return <div>Notes Page</div>;
+export default async function NotesPage() {
+  const { userId } = auth();
+
+  if (!userId) throw Error("userId undefined");
+
+  const allNotes = await prisma.note.findMany({ where: { userId } });
+
+  return <div>{JSON.stringify(allNotes)}</div>;
 }
