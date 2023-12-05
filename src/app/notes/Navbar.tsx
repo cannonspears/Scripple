49eb44104aeabbd6f97288a54d5c7f5e0688d877
Cannon Spears <cannonspears@gmail.com>
2023-12-05 09:57:25 -0600

    Added logo and styled

--- src/app/notes/Navbar.tsx
+++ src/app/notes/Navbar.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+
 export default function NavBar() {
   return (
     <div className="p-4 shadow">
       <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
-        <span>Element 1</span>
+        <Link href="/notes" className="flex items-center gap-3">
+          <Image src={logo} alt="notesGPT logo" width={40} height={40} />
+          <span className="font-bold">notesGPT</span>
+        </Link>
         <span>Element 2</span>
       </div>
     </div>
