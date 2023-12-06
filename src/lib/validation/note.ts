2355a3d90cfa21323bdec5b2c309653e61620b51
Cannon Spears <cannonspears@gmail.com>
2023-12-06 11:09:41 -0600

    Update note request

--- src/lib/validation/note.ts
+++ src/lib/validation/note.ts
@@ -6,3 +6,7 @@ export const createNoteSchema = z.object({
 });
 
 export type CreateNoteSchema = z.infer<typeof createNoteSchema>;
+
+export const updateNoteSchema = createNoteSchema.extend({
+  id: z.string().min(1),
+});
