9dceb084b0e59122aca1e3ec02b43113de759c76
Cannon Spears <cannonspears@gmail.com>
2023-12-12 08:42:40 -0600

    Create chatbox

---
+++ src/components/AIChatBox.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import { useChat } from "ai/react";
+
+interface AIChatBoxProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    setMessages,
+    isLoading,
+    error,
+  } = useChat();
+
+  return <div>Chatbox</div>;
+}
