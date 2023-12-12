ad9d17d699842fb9b9e46a4b7abce5f859eec748
Cannon Spears <cannonspears@gmail.com>
2023-12-12 12:01:36 -0600

    Add user icon and style

--- src/components/AIChatBox.tsx
+++ src/components/AIChatBox.tsx
@@ -1,9 +1,11 @@
 import { cn } from "@/lib/utils";
 import { useChat } from "ai/react";
-import { XCircle } from "lucide-react";
+import { Bot, XCircle } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Message } from "ai";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 interface AIChatBoxProps {
   open: boolean;
@@ -51,10 +53,35 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
 }
 
 function ChatMessage({ message: { role, content } }: { message: Message }) {
+  const { user } = useUser();
+
+  const isAiMessage = role === "assistant";
+
   return (
-    <div className="mb-3">
-      <div>{role}</div>
-      <div>{content}</div>
+    <div
+      className={cn(
+        "mb-3 flex items-center",
+        isAiMessage ? "me-5 justify-start" : "ms-5 justify-end",
+      )}
+    >
+      {isAiMessage && <Bot className="mr-2 shrink-0" />}
+      <p
+        className={cn(
+          "whitespace-pre-line rounded-md border px-3 py-2",
+          isAiMessage ? "bg-background" : "bg-primary text-primary-foreground",
+        )}
+      >
+        {content}
+      </p>
+      {!isAiMessage && user?.imageUrl && (
+        <Image
+          src={user.imageUrl}
+          alt="User image"
+          width={100}
+          height={100}
+          className="ml-2 h-10 w-10 rounded-full object-cover"
+        />
+      )}
     </div>
   );
 }
