dae55252e15188f7cfd0a9ab4c5ff4a78fc31e70
Cannon Spears <cannonspears@gmail.com>
2023-12-27 14:29:58 -0600

    Updated user prompting

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,7 +1,9 @@
 import { auth } from "@clerk/nextjs";
 import prisma from "@/lib/db/prisma";
 import { Metadata } from "next";
+import fullLogo from "@/assets/full-logo-tp.png";
 import Note from "@/components/Note";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Scripple | Home",
@@ -15,13 +17,22 @@ export default async function NotesPage() {
   const allNotes = await prisma.note.findMany({ where: { userId } });
 
   return (
-    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3 ">
       {allNotes.map((note) => (
         <Note note={note} key={note.id} />
       ))}
       {allNotes.length === 0 && (
-        <div className="col-span-full text-center">
-          {"You don't have any Scripples yet."}
+        <div className="col-span-full flex flex-col items-center justify-center">
+          <p className="mb-4">{"You don't have any Scripples yet!"}</p>
+          <p className="mb-4">
+            {"Add helpful context like topics, style, or scripts."}
+          </p>
+          <Image
+            src={fullLogo}
+            alt="Scripple mascot"
+            width={300}
+            height={300}
+          />
         </div>
       )}
     </div>
